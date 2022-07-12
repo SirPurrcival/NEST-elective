b502eca47adf8939463b1fdd37a53d8e8050593f
SirPurrcival <be.pauli@student.maastrichtuniversity.nl>
2022-07-12 12:16:34 +0200

    Working version!

--- functions.py
+++ functions.py
@@ -68,7 +68,6 @@ class Network():
         # Then we connect the the neurons to the spike detectors
         # Note: You can use slicing for nest node collections as well
         ## Your code here
-        print(len(self.neurons_in[:self.n_rec_in]), self.n_rec_in)
         nest.Connect(self.neurons_ex[:self.n_rec_ex], self.spike_recorder_ex)
         nest.Connect(self.neurons_in[:self.n_rec_in], self.spike_recorder_in)
 
@@ -78,16 +77,13 @@ class Network():
 
     def get_data(self):
         # Define lists to store spike trains in
-        self.spikes_ex = []
-        self.spikes_in = []
+        # self.spikes_ex = []
+        # self.spikes_in = []
         
         self.spike_times_ex = nest.GetStatus(self.spike_recorder_ex)
         self.spike_times_in = nest.GetStatus(self.spike_recorder_in)
         
-        self.tmp_in = np.empty(self.n_rec_in, dtype=object)
-        self.tmp_in[...] = [[] for _ in range(self.tmp_in.shape[0])]
-        self.tmp_ex = np.empty(self.n_rec_ex, dtype=object)
-        self.tmp_ex[...] = [[] for _ in range(self.tmp_ex.shape[0])]
+
         # There are several ways in which you can obtain the data recorded by the spikerecorders
         # One example is given below.
         # You can get the recorded quantities from the spike recorder with nest.GetStatus
@@ -95,15 +91,22 @@ class Network():
         # you might want to sort the spike times, they are not by default
         ## Your code here
         
-        for i in range(len(self.spike_times_ex[0]['events']['senders'])):
-            idx = self.spike_times_ex[0]['events']['senders'][i] - min(self.spike_times_ex[0]['events']['senders'])
-            self.tmp_ex[idx].append(self.spike_times_ex[0]['events']['times'][i])
-            
-        for i in range(len(self.spike_times_in[0]['events']['senders'])):
-            idx = self.spike_times_in[0]['events']['senders'][i] - min(self.spike_times_in[0]['events']['senders'])
-            self.tmp_in[idx].append(self.spike_times_in[0]['events']['times'][i])
-            
         
+        self.idx_ex = np.argsort(self.spike_times_ex[0]['events']['senders'])
+        self.sorted_sx = self.spike_times_ex[0]['events']['senders'][self.idx_ex]
+        self.tmp_ex = self.spike_times_ex[0]['events']['times'][self.idx_ex]
+
+        self.spikes_ex = np.split(self.tmp_ex, np.where(np.diff(self.sorted_sx) > 0)[0] + 1)
+        for a in self.spikes_ex:
+            a.sort()
+        
+        self.idx_in = np.argsort(self.spike_times_in[0]['events']['senders'])
+        self.sorted_si = self.spike_times_in[0]['events']['senders'][self.idx_in]
+        self.tmp_in = self.spike_times_in[0]['events']['times'][self.idx_in]
+
+        self.spikes_in = np.split(self.tmp_in, np.where(np.diff(self.sorted_si) > 0)[0] + 1)
+        for b in self.spikes_in:
+            b.sort()
         #for item in self.spike_times_ex:
         #    self.spikes_ex.append(np.sort(item['events']['times']))
         # 
@@ -117,7 +120,7 @@ class Network():
         #      times   = nest.GetStatus(self.spikes_recorder, 'events')[0]['times']
         # Try to practice with the nest.GetStatus command.
         
-        return self.tmp_ex, self.tmp_in
+        return self.spikes_ex, self.spikes_in
 
 # Helper functions
 def raster(spikes_ex, spikes_in, rec_start, rec_stop, figsize=(9, 5)):
@@ -180,51 +183,3 @@ def rate(spikes_ex, spikes_in, rec_start, rec_stop):
                            /(n_rec_ex + n_rec_in))
     print(f'Average firing rate: {average_firing_rate} Hz')
 
-neuron_params = {"C_m":     1.0,
-                 "tau_m":   20.,
-                 "t_ref":   2.0,
-                 "E_L":     0.0,
-                 "V_reset": 0.0,
-                 "V_m":     0.0,
-                 "V_th":    20.
-                 }
-
-params = {
-    'num_neurons': 8000,                # number of neurons in network
-    'rho':  0.2,                        # fraction of inhibitory neurons
-    'eps':  0.2,                        # probability to establish a connections
-    'g':    5,                          # excitation-inhibition balance
-    'eta':  3.5,                          # relative external rate
-    'J':    0.1,                        # postsynaptic amplitude in mV
-    'neuron_params': neuron_params,     # single neuron parameters
-    'n_rec_ex':  600,                   # excitatory neurons to be recorded from
-    'n_rec_in':  150,                   # inhibitory neurons to be recorded from
-    'rec_start': 600.,                  # start point for recording spike trains
-    'rec_stop':  800.                   # end points for recording spike trains
-    }
-
-nest.ResetKernel()
-nest.SetKernelStatus({'local_num_threads': 4})  # Adapt if necessary
-
-nest.print_time = True
-#nest.overwrite_files = True
-
-network = Network(**params)
-network.create()
-network.simulate(1000)
-test = network.get_data()
-rate(test[0], test[1], params.get('rec_start'), params.get('rec_stop'))
-
-#nu_th = theta / (J * CE * tauMem)
-#nu_ex = eta * nu_th
-
-#theta = V_th
-
-nu_th = neuron_params['V_th'] / (params['J'] * network.c_ex * neuron_params['tau_m'])
-nu_ex = params['eta'] * nu_th
-
-ratio = nu_ex/nu_th
-print(f"Parameters for this network: Nu_ex/Nu_thr = {ratio}, g = {params['g']}, Delay = 1.5ms")
-
-raster(test[0], test[1], params.get('rec_start'), params.get('rec_stop'))
-#nest.raster_plot.from_device(network.spike_recorder_ex)
---
+++ run.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 12:22:23 2022
+
+@author: meowlin
+"""
+
+# Import libraries
+from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import nest
+import itertools
+
+from functions import Network, raster, rate
+
+neuron_params = {"C_m":     1.0,
+                 "tau_m":   20.,
+                 "t_ref":   2.0,
+                 "E_L":     0.0,
+                 "V_reset": 0.0,
+                 "V_m":     0.0,
+                 "V_th":    20.
+                 }
+
+params = {
+    'num_neurons': 8000,                # number of neurons in network
+    'rho':  0.2,                        # fraction of inhibitory neurons
+    'eps':  0.2,                        # probability to establish a connections
+    'g':    5,                          # excitation-inhibition balance
+    'eta':  5,                          # relative external rate
+    'J':    0.1,                        # postsynaptic amplitude in mV
+    'neuron_params': neuron_params,     # single neuron parameters
+    'n_rec_ex':  600,                   # excitatory neurons to be recorded from
+    'n_rec_in':  150,                   # inhibitory neurons to be recorded from
+    'rec_start': 600.,                  # start point for recording spike trains
+    'rec_stop':  800.                   # end points for recording spike trains
+    }
+
+nest.ResetKernel()
+nest.SetKernelStatus({'local_num_threads': 4})  # Adapt if necessary
+
+nest.print_time = True
+#nest.overwrite_files = True
+
+network = Network(**params)
+network.create()
+network.simulate(1000)
+test = network.get_data()
+rate(test[0], test[1], params.get('rec_start'), params.get('rec_stop'))
+
+#nu_th = theta / (J * CE * tauMem)
+#nu_ex = eta * nu_th
+
+#theta = V_th
+
+nu_th = neuron_params['V_th'] / (params['J'] * network.c_ex * neuron_params['tau_m'])
+nu_ex = params['eta'] * nu_th
+
+ratio = nu_ex/nu_th
+
+
+
+raster(test[0], test[1], params.get('rec_start'), params.get('rec_stop'))
+#nest.raster_plot.from_device(network.spike_recorder_ex)
